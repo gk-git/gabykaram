1f7b8a748cfc2fbd82abe5f0790a1d7c80aa0f0d
Gaby Karam <info@gabykaram.com>
2021-09-29 14:05:42 +0300

    Capitalize my name, even that I like it to stay lowercase (lets be down to earth)

--- src/components/Global/Intro.js
+++ src/components/Global/Intro.js
@@ -5,8 +5,8 @@ const Intro = () => {
   return (
     <div id="intro" className="fadeWhiteGradient">
       <div className="content">
-        <h1 className="animateOnFirstLoad">gab<span className="char1">y</span>&nbsp;<span
-          className="char2">k</span>ar<span
+        <h1 className="animateOnFirstLoad">Gab<span className="char1">y</span>&nbsp;<span
+          className="char2">K</span>ar<span
           className="char3">a</span>m</h1>
         <h2 className="animateOnFirstLoad">Digital Producer & Developer</h2>
       </div>
