741a6519f8664033d2d0f33d9e216534a5b95729
Gaby Karam <info@gabykaram.com>
2021-10-03 19:42:45 +0300

    Change the webring style and adding new icons set

--- src/components/Global/WebRingNavigation.js
+++ src/components/Global/WebRingNavigation.js
@@ -1,30 +1,32 @@
+import { StaticImage } from "gatsby-plugin-image"
 import React from "react"
 import webRingMembers from "../../../content/webring.json"
 
-const getRandomInt = (min, max, notIn = []) => {
-  min = Math.ceil(min)
-  max = Math.floor(max)
-  let randomInt = Math.floor(Math.random() * (max - min + 1)) + min
-  if (notIn.indexOf(randomInt) >= 0) {
-    randomInt = getRandomInt(min, max, notIn)
-  }
-  return randomInt
-}
 const WebRingNavigation = () => {
   const prevSiteIndex = webRingMembers.members.length - 1;
   const nextSiteIndex = 1;
-  const randomSiteIndex = getRandomInt(1, webRingMembers.members.length - 1, [0])
   
   return (
     <div className="webring">
-      <p>Other Members in my community</p>
-      <div id="copy">
-        <p>
-          <a href={webRingMembers.members[prevSiteIndex]?.url}>Prev</a>
-          <a href={webRingMembers.members[nextSiteIndex]?.url}>Next</a>
-          <a href={webRingMembers.members[randomSiteIndex]?.url}>Random</a>
-        </p>
-      </div>
+      <p >
+        {/* webRingMembers.members[prevSiteIndex]?.url */}
+        <span >A proud member of Batata Harra Webring</span>
+        <a title={`Visit ${webRingMembers.members[prevSiteIndex]?.url}`} aria-label={`visit ${webRingMembers.members[prevSiteIndex]?.name} Dot Com`}
+           href={webRingMembers.members[prevSiteIndex]?.url}>
+          <i className="icon-left-lane"/>
+        </a>
+        <a rel="noopener" title="Batata Harra Webring" aria-label="visit Batata Harra Dot Guru"
+           className="text-decoration-none m-1" href="https://batataharra.guru" target="_blank">
+              <span>
+                <img className="harra" src="https://abouhanna.com/images/flame.gif" alt="Flame"/>
+                <span className="batata">🥔</span>
+              </span>
+        </a>
+        <a title={`Visit ${webRingMembers.members[nextSiteIndex]?.url}`} aria-label={`visit ${webRingMembers.members[nextSiteIndex]?.name} Dot Com`}
+           href={webRingMembers.members[nextSiteIndex]?.url}>
+          <i className="icon-right-lane"/>
+        </a>
+      </p>
     </div>
   )
 }
