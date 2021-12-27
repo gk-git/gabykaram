9d39cac1dea014f700941e8229ade910ea49a64f
Gaby Karam <info@gabykaram.com>
2021-12-27 10:27:39 +0200

    Update Parallax footer to use less code

--- src/components/Global/Layout.js
+++ src/components/Global/Layout.js
@@ -15,111 +15,28 @@ const Layout = ({
                   isSinglePost = false,
                   className = ""
                 }) => {
-  const [state, setState] = useState({
-    placeholderTop: undefined,
-    ticking: undefined
-  })
   const { navigation } = useContext(AppContext)
   const parallaxPlaceholder = useRef()
   const footer = useRef()
   const windowSize = useWindowSize()
-  let ts
   
-  useEffect(() => {
-  
-    "wheel".split(" ").forEach(event => {
-      window.addEventListener(event, handleScrollOverFixedFooter)
-    
-    })
-    document.querySelector("footer").addEventListener("touchstart", handleTouchScrollOverFixedFooterStart)
-    document.querySelector("footer").addEventListener("touchmove", handleTouchScrollOverFixedFooterEnd)
-    return () => {
-      "wheel".split(" ").forEach(event => {
-        window.removeEventListener(event, handleScrollOverFixedFooter)
-      })
-      document.querySelector("footer").removeEventListener("touchstart", handleTouchScrollOverFixedFooterStart)
-      document.querySelector("footer").removeEventListener("touchmove", handleTouchScrollOverFixedFooterEnd)
-    
-    }
-  });
   
   const checkFooterHeight = () => {
-    
     if (footer.current.offsetHeight > window.innerHeight) { // Check if footer is taller than window height
-      window.addEventListener("scroll", onScroll)
       footer.current.style.bottom = "unset"
       footer.current.style.top = "0px"
     } else { // If footer height is not greater than window height, bottom is 0 for normal parllax
-      window.removeEventListener("scroll", onScroll)
       footer.current.style.top = "unset"
       footer.current.style.bottom = "0px"
     }
   }
   useEffect(() => {
-    updateHolderHeight()
     checkFooterHeight()
   }, [])
   useEffect(() => {
-    // console.log("using effect")
-    updateHolderHeight()
     checkFooterHeight()
-    
   }, [windowSize])
   
-  const updateHolderHeight = () => {
-    // Placeholder should always match footer height
-    parallaxPlaceholder.current.style.height = `${footer.current.offsetHeight}px`
-  }
-  const handleScrollOverFixedFooter = (event) => {
-    if (event.defaultPrevented) {
-      // This is not a passive event
-      event.preventDefault()
-    }
-    document.querySelector("body").scrollTop += event?.deltaY || 0
-  }
-  
-  const handleTouchScrollOverFixedFooterStart = (event) => {
-    if (event?.touches[0]?.clientY) {
-      ts = event.touches[0].clientY
-    }
-  }
-  
-  const handleTouchScrollOverFixedFooterEnd = (event) => {
-    const te = event.changedTouches[0].clientY
-    if (ts < te) {
-      document.querySelector("body").scrollTop -= 8
-    }
-  }
-  
-  const onScroll = () => {
-    setState({
-      ...state,
-      placeholderTop: Math.round(parallaxPlaceholder.current.getBoundingClientRect().top)
-    })
-    requestTick()
-  }
-  
-  const requestTick = () => {
-    if (!state.ticking) requestAnimationFrame(updateBasedOnScroll)
-    setState({
-      ...state,
-      ticking: true
-    })
-  }
-  
-  
-  const updateBasedOnScroll = ()=> {
-    // Reset the tick so we can capture the next onScroll
-    setState({
-      ticking: false
-    })
-    
-    // When main content disappears from view, start to move footer up
-    // in conjunction with placeholder top value (in relation to viewport)
-    if (state.placeholderTop <= 0) {
-      footer.current.style.top = `${state.placeholderTop}px` // match footer top value with placeholder's top value
-    }
-  }
   return (
     <div className={`default-layout ${className} ${navigation.isOpen ? "open" : "close"}`}>
       <Helmet
@@ -153,8 +70,6 @@ const Layout = ({
             </>
           )
         }
-  
-  
       </main>
       <div className="parallax-placeholder" ref={parallaxPlaceholder} />
       <footer ref={footer}>
