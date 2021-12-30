49b3b73f4f9bb0422e92e7900a502d9f218dc6f5
Gaby Karam <info@gabykaram.com>
2021-12-30 12:23:02 +0200

    Using gatsby plugin gatsby-plugin-google-gtag to add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,7 +13,36 @@ const {
 const isNetlifyProduction = NETLIFY_ENV === "production"
 const siteUrl = isNetlifyProduction ? NETLIFY_SITE_URL : NETLIFY_DEPLOY_URL;
 
-
+const productionConfig = [
+  {
+    resolve: `gatsby-plugin-google-gtag`,
+    options: {
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "G-9QQP7EWQ8G", // Google Analytics / GA
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        anonymize_ip: true,
+        cookie_expires: 0
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"]
+      }
+    }
+  }
+]
+let plugins = [];
+if(process.env.NODE_ENV === "production") {
+  plugins = productionConfig;
+}
 module.exports = {
   /* Your site config here */
   siteMetadata: {
@@ -80,5 +109,6 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
+    ...plugins
   ]
 }
