9d3d630dba7f34800c9e0166d2a303b06df5c1c7
Gaby Karam <info@gabykaram.com>
2022-01-06 18:16:26 +0200

    Add MusicNoteIcon.tsx for mobile

---
+++ src/components/Global/CurrentlyPlaying/MusicNoteIcon.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+import "./MusicNoteIcon.scss"
+export default function MusicNoteIcon({ className = "" }) {
+  return (
+    <svg xmlns='http://www.w3.org/2000/svg' className={`music-icon ${className}`} viewBox='0 0 415.07 454.27'>
+      <g>
+        <path
+          d='M478.55 68.92l-.41 262-16.55.21-.75-242.57-230.32 57.74v-12.9z'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M230.52 146v141.73h25.3l.74-151.2z'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          className="animate-colours"
+          d='M253.4 230.06L461.59 197l-.11 9.53-208.35 48z'
+          transform='translate(-85.91 -68.27)'
+        />
+
+      </g>
+
+      <g className="left-group">
+        <path
+          className="animate-left-down"
+          d='M230.4 286.08v82.76H255l.734-88.29z'
+          data-name='path926'
+          transform='translate(-85.8 -68)'
+        />
+        <path
+          d='M252.34 357.28c12 20.65-8.69 51.79-46.12 69.56s-77.46 15.42-89.42-5.23 8.69-51.8 46.12-69.56 77.46-15.42 89.42 5.23z'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          className="white"
+          d='M143.15 423.24c-21.33-2.49-22-19.78-1.54-38.62S196 352.54 217.32 355a30.34 30.34 0 016.9 1.56'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M144.19 425.29c3.33-21.32 38.46-51 78.48-66.23 4.29-1.63 8.54-3.06 12.7-4.27'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M92.71 387.77c-1.47-32.15 26.05-64 61.47-71.07 13.25-2.66 26.34-1.53 37.48 3.24'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M86.37 398.05c15.15-37.14 65.4-71.23 121.76-82.61'
+          transform='translate(-85.91 -68.27)'
+        />
+      </g>
+      {/**/}
+      <g className="right-group">
+        <path
+          className="animate-right-down"
+          d='M461.52 329.64v88.29h16.55l.48-94.18z'
+          data-name='path926'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M474.85 396c12.51 23.85-9.08 59.81-48.24 80.32s-81 17.81-93.54-6 9.09-59.81 48.24-80.32 81.04-17.82 93.54 6z'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          className="white"
+          d='M361.77 473.32c-22.31-2.87-23-22.83-1.61-44.58s56.89-37.05 79.2-34.17a29.35 29.35 0 017.22 1.8'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M362.86 475.7c3.48-24.62 40.23-58.86 82.09-76.48 4.49-1.89 8.94-3.54 13.29-4.93'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M496 435.11C500.11 467 475.32 501 440.61 511c-13 3.75-26.12 3.71-37.62-.12'
+          transform='translate(-85.91 -68.27)'
+        />
+        <path
+          d='M500.5 429.43c-13.05 39.67-60.44 77.9-114.85 92.63'
+          transform='translate(-85.91 -68.27)'
+        />
+      </g>
+    </svg>
+  )
+}
