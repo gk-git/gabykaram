412aa51b9642bdbf3110585e9cd3151cecb9bd4c
Gaby Karam <info@gabykaram.com>
2022-01-06 23:07:37 +0200

    Adding LiveIcon component

---
+++ src/components/Global/CurrentlyPlaying/atoms/LiveIcon.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+import "./LiveIcon.scss"
+export default function LiveIcon() {
+  return (
+    <div className="live__icon">
+      <div className="circle--outer" />
+      <div className="circle--inner" />
+    </div>
+  )
+}
