94976473ba08f518663e2f6aa080ddcbb3d2ba47
Gaby Karam <info@gabykaram.com>
2022-04-21 23:56:51 +0300

    test 2

--- lambda/users.js
+++ lambda/users.js
@@ -1,17 +1,17 @@
+// lambda/airtable.js
 const Airtable = require('airtable')
 
+Airtable.configure({
+  endpointUrl: 'https://api.airtable.com',
+  apiKey: process.env.AIRTABLE_KEY
+})
+const base = Airtable.base('appiEYG2OPWKTwP61')
+
 exports.handler = function(event, context, callback) {
-  Airtable.configure({
-    endpointUrl: process.env.AIRTABLE_API_URL,
-    apiKey: process.env.AIRTABLE_API_KEY
-  })
-  
-  const base = Airtable.base('appiEYG2OPWKTwP61')
   const allRecords = []
-  base('entries')
+  base('Users')
     .select({
-      // This caps total records to 100, not just each request
-      // maxRecords: 100,
+      maxRecords: 100,
       view: 'all'
     })
     .eachPage(
@@ -25,14 +25,13 @@ exports.handler = function(event, context, callback) {
         if (err) {
           callback(err)
         } else {
-          console.log(allRecords.length)
           const body = JSON.stringify({ records: allRecords })
           const response = {
             statusCode: 200,
             body: body,
             headers: {
               'content-type': 'application/json',
-              'cache-control': 'Cache-Control: max-age=60, public'
+              'cache-control': 'Cache-Control: max-age=300, public'
             }
           }
           callback(null, response)
