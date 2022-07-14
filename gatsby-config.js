b3719a6c1c22878dd0feefd067742147dfd04b80
Gaby K <gaby@polarstork.com>
2022-07-15 02:01:01 +0300

    adding GTM and GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -106,21 +106,44 @@ module.exports = {
         path: `${__dirname}/src/assets/images`,
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-csp`,
-    //   options: {
-    //     disableOnDev: true,
-    //     reportOnly: true, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-    //     mergeScriptHashes: false,
-    //     mergeStyleHashes: false,
-    //     directives: {
-    //       'script-src': `'self' 'unsafe-inline' data: www.googletagmanager.com googletagmanager.com www.google-analytics.com`,
-    //       'style-src': `'self' 'unsafe-inline'`,
-    //       'img-src': `'self' data: www.googletagmanager.com googletagmanager.com www.google-analytics.com`,
-    //       'font-src': `'self' data:`,
-    //       'connect-src': `'self' https://www.google-analytics.com`
-    //     },
-    //   }
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-9QQP7EWQ8G",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Enables Google Optimize using your container Id
+        optimizeId: "YOUR_GOOGLE_OPTIMIZE_TRACKING_ID",
+        // Enables Google Optimize Experiment ID
+        experimentId: "YOUR_GOOGLE_EXPERIMENT_ID",
+        // Set Variation ID. 0 for original 1,2,3....
+        variationId: "YOUR_GOOGLE_OPTIMIZE_VARIATION_ID",
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // Any additional optional fields
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "example.com",
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-google-tagmanager",
+      options: {
+        id: "GTM-TWBXQFG",
+        includeInDevelopment: false,
+        enableWebVitalsTracking: true,
+      },
+    },
   ]
 }
